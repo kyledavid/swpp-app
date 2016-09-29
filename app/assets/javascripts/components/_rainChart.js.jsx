cba11520b190a700cfd466488911bc20476a5c6d
kyledavid <kyledavid022@gmail.com>
2016-09-29 19:37:12 +0000

    cleaned up _rainChart

--- app/assets/javascripts/components/_rainChart.js.jsx
+++ app/assets/javascripts/components/_rainChart.js.jsx
@@ -18,42 +18,53 @@ var compareAsc = function(a, b) {
     }
 }
 
+
 var RainChart = React.createClass({
     getInitialState() {
-      return {desc: true};  
+      return {
+          desc: true,
+      };  
     },
     handleDescToggle() {
         this.setState({desc: !this.state.desc});
         console.log(this.state.desc);
     },
-    render() {
-        var sites = this.props.sites;
-        
-        var comps = [];
-        
+    _sortSites(sites) {
         if(this.state.desc){
             sites.sort(compareDesc);
         } else {
             sites.sort(compareAsc);
         }
         
-        
-        console.log(sites);
+        return sites;
+    },
+    _createChartRows(sites) {
+        var comps = [];
         
         sites.forEach(function(site){
             comps.push(<ChartRow site={site} key={site.name}/>);
         });
+        
+        return comps;
+    },
+    render() {
+        var sites = this.props.sites;
+        var comps = [];
+        
+        sites = this._sortSites(sites);
+        comps = this._createChartRows(sites);
+        
         return (
         	<div className="chart-panel">
         		<h2 className="zip-title">Rainfall</h2>
-        	
         		<table className="rain-table">
         			
-        
         			<ChartHeading descTogg={this.handleDescToggle}></ChartHeading>
         			<tbody>{comps}</tbody>
 
         		</table>
+        		<pre>{JSON.stringify(this.state, null, 2)}<br/>{JSON.stringify(this.props, null, 2)}</pre>
+        		<pre></pre>
         	</div>// chart panel
             );
     }
