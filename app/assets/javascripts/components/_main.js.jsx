8ed3963e13eacd99023900a47324808de06db384
kyledavid <kyledavid022@gmail.com>
2016-10-04 00:52:04 +0000

    Added comments. Used arrow functions where appropriate

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -1,34 +1,36 @@
 // app/assets/javascripts/components/_main.js.jsx 
+
+// collect the rain data stored in the DOM
 var collateSiteData = function(sites) {
+    // get all elements with 'rains' class
     var rains = document.getElementsByClassName('rains');
-        var rainOfSites = Array.prototype.map.call(rains, function(obj){
-           return {rain: JSON.parse(obj.dataset.rain), id: JSON.parse(obj.dataset.siteId)}
-         });
-        console.log(rainOfSites);
+        // for each rain element return the rain and id values in a new javascript object
+        var rainOfSites = Array.prototype.map.call(rains, obj => ({rain: JSON.parse(obj.dataset.rain), id: JSON.parse(obj.dataset.siteId)}));
+        // combine the harvested rain values with the appropriate job sites
         return addRainToSites(sites, rainOfSites);
 }
 
 var addRainToSites = function(sites, rainOfSites) {
-    var updatedSites = sites.map(function(site){
-        var siteId = site.id;
-        
-        var siteRains = rainOfSites.filter(function(siteRain){
-            return siteRain.id === siteId;
-        });
-        
+    // cycle through array of sites, returns array with rain values appended to sites
+    var updatedSites = sites.map(site => {
+        let siteId = site.id;
+        // get the the array of rain values matching a particular siteId
+        let siteRains = rainOfSites.filter(siteRain => siteRain.id === siteId);
+        // append rain data to site
         site.rain = siteRains[0].rain;
         
         return site;
-        
     });
-    
+    // now strip down the site to the essentials
     return stripDownSites(updatedSites);
 }
 
 var stripDownSites = function(updatedSites) {
-    var strippedSites = updatedSites.map(function(site){
+    // now that rainfall has been matched with the appropriate site, get the mean of the rainfall values and strip object down to the essential values
+    var strippedSites = updatedSites.map(site => {
+        // send rainfall array and return the average
         var averageRain = averageRainFall(site.rain);
-        
+        // create our new site object
         var newSite = {
             name: site.job_name,
             address: site.cross_street,
@@ -38,10 +40,10 @@ var stripDownSites = function(updatedSites) {
         
         return newSite;
     });
-    
+    // return array of processed sites
     return strippedSites;
 }
-
+// get the average rate of rain based on a set of rain readings for a particular site
 var averageRainFall = function(rains) {
     
     var average = rains.reduce(function(p, c){
@@ -51,11 +53,12 @@ var averageRainFall = function(rains) {
     average = average / rains.length;
     
     return average;
-    
+    // uncomment this to test table sorting with random rain values
     // return Math.round(Math.random() * 10) / 10;
 }
 
 const Main = ({sites}) => {
+    // combine raw site information with rain data and return as a simple object
     var formattedSites = collateSiteData(sites);
     
     return (
