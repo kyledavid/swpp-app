720c17861def662dd2b454dd17be0547ae7f8f99
kyledavid <kyledavid022@gmail.com>
2016-10-07 23:03:57 +0000

    snatch up any loose changes

--- app/assets/javascripts/components/_rainChart.js.jsx
+++ app/assets/javascripts/components/_rainChart.js.jsx
@@ -1,26 +1,3 @@
-var comparisonFunctions = (function() {
-    return {
-        compareAsc: function(a, b) {
-            if (a.rain > b.rain ){
-                return 1;
-            } else if (a.rain < b.rain) {
-                return -1;   
-            } else {
-                return 0;
-            }
-        },
-        compareDesc: function(a, b) {
-            if (a.rain > b.rain ){
-                return -1;
-            } else if (a.rain < b.rain) {
-                return 1;   
-            } else {
-                return 0;
-            }
-        }
-    };
-})();
-
 const RainChart = React.createClass({
     getInitialState() {
       return {
