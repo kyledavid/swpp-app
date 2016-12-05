048889db9f1e7e6a8f5cbf7a70135e3dab1ef2f7
Neal H <nealh.dev@gmail.com>
2016-12-05 23:41:33 +0000

    update

--- db/migrate/20160923234415_add_job_sites_id_to_rains.rb
+++ db/migrate/20160923234415_add_job_sites_id_to_rains.rb
@@ -1,6 +1,6 @@
 class AddJobSitesIdToRains < ActiveRecord::Migration
   def change
       #Below was added later in seperate migration.  Uncomment to use. But only here for reference.
-      add_reference :rains, :job_sites_id, index: true, foreign_key: true
+      add_reference :rains, :job_sites, index: true, foreign_key: true
   end
 end
