be98a4460275fabe78b1d665eb25332a0360d977
Neal H <nealh.dev@gmail.com>
2016-12-05 23:27:39 +0000

    modification to migration file

--- db/migrate/20160923234412_create_rains.rb
+++ db/migrate/20160923234412_create_rains.rb
@@ -3,7 +3,7 @@ class CreateRains < ActiveRecord::Migration
     create_table :rains do |t|
       t.string :amount
       #Below was added later in seperate migration.  Uncomment to use. But only here for reference.
-      #t.references :job_site_id, index: true, foreign_key: true
+      t.references :job_site_id, index: true, foreign_key: true
 
       t.timestamps null: false
     end
