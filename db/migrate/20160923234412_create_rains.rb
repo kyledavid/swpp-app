3621f86c98872451f96bd2c06f150f7315d99061
Neal H <nealh.dev@gmail.com>
2016-12-05 22:35:43 +0000

    modified create rains migration

--- db/migrate/20160923234412_create_rains.rb
+++ db/migrate/20160923234412_create_rains.rb
@@ -2,7 +2,8 @@ class CreateRains < ActiveRecord::Migration
   def change
     create_table :rains do |t|
       t.string :amount
-      t.references :job_site_id, index: true, foreign_key: true
+      #Below was added later in seperate migration.  Uncomment to use. But only here for reference.
+      #t.references :job_site_id, index: true, foreign_key: true
 
       t.timestamps null: false
     end
