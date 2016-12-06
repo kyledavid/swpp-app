f218fd7133dab2c8e82e7f15c4ad6443310e3ffc
Neal H <nealh.dev@gmail.com>
2016-12-06 16:50:56 +0000

    precompiled assets

--- db/migrate/20160923234415_add_job_sites_id_to_rains.rb
+++
@@ -1,6 +0,0 @@
-class AddJobSitesIdToRains < ActiveRecord::Migration
-  def change
-      #Below was added later in seperate migration.  Uncomment to use. But only here for reference.
-      add_column :rains, :job_sites_id, :integer
-  end
-end
